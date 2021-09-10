22a0e380ab5658aff2734737e8dd2477da1b4824
SulthanRafif <rsulthan5@gmail.com>
2021-09-10 17:03:07 +0700

    Menambah level user

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\GuruController;
 use App\Http\Controllers\SiswaController;
 use App\Http\Controllers\PenjualanController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PelangganController;
 
 
 // Route::get('/', function () {
@@ -41,26 +43,36 @@ Route::view('/contact', 'contact');
 // ---------------------------------------------------------
 
 Route::get('/', [HomeController::class, 'index']);
-
-Route::get('/guru', [GuruController::class, 'index'])->name('guru');
-Route::get('/guru/detail/{id_guru}', [GuruController::class, 'detail']);
-Route::get('/guru/add', [GuruController::class, 'add']);
-Route::post('/guru/insert', [GuruController::class, 'insert']);
-Route::get('/guru/edit/{id_guru}', [GuruController::class, 'edit']);
-Route::post('/guru/update/{id_guru}', [GuruController::class, 'update']);
-Route::get('/guru/delete/{id_guru}', [GuruController::class, 'delete']);
-
-Route::get('/siswa', [SiswaController::class, 'index']);
-Route::get('/penjualan', [PenjualanController::class, 'index']);
-Route::get('/penjualan/print', [PenjualanController::class, 'print']);
-Route::get('/penjualan/printpdf', [PenjualanController::class, 'printpdf']);
-Route::view('/user', 'v_user');
 Route::get('/home/about/{id}', [HomeController::class, 'about']);
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// hak akses untuk admin
+Route::group(['middleware' => 'admin'], function () {
+    // user
+    Route::get('/guru', [GuruController::class, 'index'])->name('guru');
+    Route::get('/guru/detail/{id_guru}', [GuruController::class, 'detail']);
+    Route::get('/guru/add', [GuruController::class, 'add']);
+    Route::post('/guru/insert', [GuruController::class, 'insert']);
+    Route::get('/guru/edit/{id_guru}', [GuruController::class, 'edit']);
+    Route::post('/guru/update/{id_guru}', [GuruController::class, 'update']);
+    Route::get('/guru/delete/{id_guru}', [GuruController::class, 'delete']);
 
-Auth::routes();
+    // siswa
+    Route::get('/siswa', [SiswaController::class, 'index']);
+});
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::group(['middleware' => 'user'], function () {
+    // User
+    Route::get('/user', [UserController::class, 'index'])->name('user');
+
+    // Penjualan
+    Route::get('/penjualan', [PenjualanController::class, 'index']);
+    Route::get('/penjualan/print', [PenjualanController::class, 'print']);
+    Route::get('/penjualan/printpdf', [PenjualanController::class, 'printpdf']);
+});
+
+Route::group(['middleware' => 'pelanggan'], function () {
+    // Pelanggan
+    Route::get('/pelanggan', [PelangganController::class, 'index'])->name('pelanggan');
+});
